472ace74a3e4a72b8f75eeda55eab4493f0ef320
deantann557 <deantann557@SDS-L-99QLG63>
2024-01-11 11:07:00 -0500

    Add project files.

---
+++ pointChaser/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace pointChaser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.P1PointLabel = new System.Windows.Forms.Label();
+            this.P2PointLabel = new System.Windows.Forms.Label();
+            this.VictoryLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // P1PointLabel
+            // 
+            this.P1PointLabel.Location = new System.Drawing.Point(162, 34);
+            this.P1PointLabel.Name = "P1PointLabel";
+            this.P1PointLabel.Size = new System.Drawing.Size(100, 23);
+            this.P1PointLabel.TabIndex = 0;
+            this.P1PointLabel.Text = "P1PointLabel";
+            // 
+            // P2PointLabel
+            // 
+            this.P2PointLabel.Location = new System.Drawing.Point(542, 34);
+            this.P2PointLabel.Name = "P2PointLabel";
+            this.P2PointLabel.Size = new System.Drawing.Size(100, 23);
+            this.P2PointLabel.TabIndex = 1;
+            this.P2PointLabel.Text = "P2PointLabel";
+            // 
+            // VictoryLabel
+            // 
+            this.VictoryLabel.Location = new System.Drawing.Point(327, 9);
+            this.VictoryLabel.Name = "VictoryLabel";
+            this.VictoryLabel.Size = new System.Drawing.Size(141, 58);
+            this.VictoryLabel.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(800, 577);
+            this.Controls.Add(this.VictoryLabel);
+            this.Controls.Add(this.P2PointLabel);
+            this.Controls.Add(this.P1PointLabel);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Point Chaser";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label P1PointLabel;
+        private System.Windows.Forms.Label P2PointLabel;
+        private System.Windows.Forms.Label VictoryLabel;
+    }
+}
+
---
+++ pointChaser/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+namespace pointChaser
+{
+
+    public partial class Form1 : Form
+    {
+        Rectangle player1 = new Rectangle(180, 170, 30, 30);
+        Rectangle player2 = new Rectangle(240, 170, 30, 30);
+        Rectangle Wall = new Rectangle( 10, 100, 580, 360);
+        Rectangle ball = new Rectangle(495, 195, 10, 10);
+        Rectangle speedBall = new Rectangle(300, 300, 10, 10);
+
+        SolidBrush blueBrush = new SolidBrush(Color.DodgerBlue);
+        SolidBrush beigeBrush = new SolidBrush(Color.Beige);
+        SolidBrush pinkBrush = new SolidBrush(Color.Pink);
+        SolidBrush yellowBrush = new SolidBrush(Color.Yellow);
+
+        Pen blackPen = new Pen(Color.Black, 15);
+
+        int player1Speed = 6;
+        int player2Speed = 6;
+
+        Random randGen = new Random();
+
+        int player1Score = 0;
+        int player2Score = 0;
+
+        SoundPlayer planeDing = new SoundPlayer(Properties.Resources.Air_Plane_Ding_SoundBible_com_496729130);
+        SoundPlayer victory = new SoundPlayer(Properties.Resources.Short_triumphal_fanfare_John_Stracke_815794903);
+
+        bool wDown = false;
+        bool sDown = false;
+        bool aDown = false;
+        bool dDown = false;
+        bool upArrowDown = false;
+        bool downArrowDown = false;
+        bool leftArrowDown = false;
+        bool rightArrowDown = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            P1PointLabel.Text = $"Player 1: {player1Score}";
+            P2PointLabel.Text = $"Player 2: {player2Score}";
+
+            e.Graphics.FillRectangle(blueBrush, player1);
+            e.Graphics.FillRectangle(beigeBrush, player2);
+            e.Graphics.DrawRectangle(blackPen, 10, 100, 580, 360);
+            e.Graphics.FillEllipse(pinkBrush, ball);
+            e.Graphics.FillEllipse(yellowBrush, speedBall);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = true;
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+                case Keys.A:
+                    aDown = true;
+                    break;
+                case Keys.D:
+                    dDown = true;
+                    break;
+                case Keys.Up:
+                    upArrowDown = true;
+                    break;
+                case Keys.Down:
+                    downArrowDown = true;
+                    break;
+                case Keys.Left:
+                    leftArrowDown = true;
+                    break;
+                case Keys.Right:
+                    rightArrowDown = true;
+                    break;
+            }
+
+        }
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
+                    break;
+                case Keys.A:
+                    aDown = false;
+                    break;
+                case Keys.D:
+                    dDown = false;
+                    break;
+                case Keys.Up:
+                    upArrowDown = false;
+                    break;
+                case Keys.Down:
+                    downArrowDown = false;
+                    break;
+                case Keys.Left:
+                    leftArrowDown = false;
+                    break;
+                case Keys.Right:
+                    rightArrowDown = false;
+                    break;
+            }
+
+
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            if (player1.IntersectsWith(ball))
+            {
+                ball.X = randGen.Next(18, 574);
+                ball.Y = randGen.Next(108, 443);
+                player1Score++;
+                planeDing.Play();
+            }
+            if (player2.IntersectsWith(ball))
+            {
+                ball.X = randGen.Next(18, 574);
+                ball.Y = randGen.Next(108, 443);
+                player2Score++;
+                planeDing.Play();
+            }
+            if (player1.IntersectsWith(speedBall))
+            {
+                speedBall.X = randGen.Next(18, 574);
+                speedBall.Y = randGen.Next(108, 443);
+                player1Speed++;
+                planeDing.Play();
+            }
+            if (player2.IntersectsWith(speedBall))
+            {
+                speedBall.X = randGen.Next(18, 574);
+                speedBall.Y = randGen.Next(108, 443);
+                player2Speed++;
+                planeDing.Play();
+            }
+
+            //move player 1
+            if (wDown == true && player1.Y > 108)
+            {
+                player1.Y -= player1Speed;
+            }
+
+            if (sDown == true && player1.Y < 423)
+            {
+                player1.Y += player1Speed;
+            }
+
+            if (aDown == true && player1.X > 19)
+            {
+                player1.X -= player1Speed;
+            }
+
+            if (dDown == true && player1.X < 550)
+            {
+                player1.X += player1Speed;
+            }
+            //move player 2
+            if (upArrowDown == true && player2.Y > 108)
+            {
+                player2.Y -= player2Speed;
+            }
+            if (downArrowDown == true && player2.Y < 423)
+            {
+                player2.Y += player2Speed;
+            }
+            if (leftArrowDown == true && player2.X > 19)
+            {
+                player2.X -= player2Speed;
+            }
+
+            if (rightArrowDown == true && player2.X <550 )
+            {
+                player2.X += player2Speed;
+            }
+
+
+            //player1Score++;
+
+            //
+            //player2Score++;
+
+            //// check score and stop game if either player is at 3
+            if (player1Score == 5)
+            {
+                gameTimer.Enabled = false;
+                VictoryLabel.Visible = true;
+                VictoryLabel.Text = "Player 1 Wins!!";
+                victory.Play();
+            }
+            else if (player2Score == 5)
+            {
+                gameTimer.Enabled = false;
+                VictoryLabel.Visible = true;
+                VictoryLabel.Text = "Player 2 Wins!!";
+                victory.Play();
+            }
+
+
+
+            Refresh();
+        }
+    }
+    }
+
